1cd38ec39bad980aa79b81e4135a356de1c27025
mharnisc <mharnisc@MC00023631.one.steria.dom>
2016-03-31 16:06:31 +0200

    Anfang mit public static void...

---
+++ Project Hot Coffee/src/TICKETSYSTEM.java
@@ -0,0 +1,14 @@
+
+public class TICKETSYSTEM {
+
+	/*
+	 * Die ausführende Klasse mit Public static void...
+	 */
+	public static void main (String args []) {
+		
+		KUNDE k = new KUNDE();
+		
+		
+	}
+	
+}
