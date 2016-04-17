96aa7a57559a62aa5b7e5cfec359a19e43db21fb
Knoxys <Knoxys@192.168.2.105>
2016-04-17 16:52:20 +0200

    Finalisierung von Version 1.0 für Heute 17.04.2016

--- Project Hot Coffee/src/TicketSystem.java
+++ Project Hot Coffee/src/TicketSystem.java
@@ -1,6 +1,6 @@
 	import java.util.Scanner;
 	
-	public class TicketSystem {
+	public class TicketSystem{
 	
 		// 1) get name
 		// 2) select movie
@@ -11,29 +11,20 @@
 		 // Beispiel für die Methode zu Eingabe Des Namen und zur ausführung des Namen
 		 // Desweiteren muss static hinter Scanner, weil Public main Void
 		 // https://www.youtube.com/watch?v=5QzyHxv-XxI
-		 
-		
-		
-		
-		
-		
+
 		static Scanner scan = new Scanner(System.in);
 		static String name;
 			// Die Stichwörter von dem der Anwender wählen kann!
 		static String[] stichwort = {"1) Blackscreen","2) Exception","3) Nullpointer","4) Anders Problem bzw. Neues Ticket eröffnen"};
 		static int nummer;
-		
-		
-		
+			
 		public static void getName(){
 			System.out.println("Bitte gib dein Name ein");
 			name=scan.nextLine();
 			System.out.println("Hallo "+name);
 			getStichwort();
 			}
-		
-		
-		
+
 		public static void getStichwort(){
 			System.out.println("Bitte wähl ein Stichwort");
 			for (int i=0;i<stichwort.length;i++){
@@ -42,24 +33,13 @@
 			int nummer=scan.nextInt();
 			System.out.println("Du hast gewähl "+stichwort[nummer-1].substring(3,stichwort[nummer-1].length()));
 			}
-		
+
 		public static void main(String [] args) {
 			//getName() ist das Wichtigste. Alles weitere kann vernachlässigt werden!
-			//boolean done = false;
-			//do{
 			try{
 			getName();
 				//if (nummer<1 || nummer>4) throw new IntegerOutOfRangeException();
 			}
-				//catch (IntegerOutOfRangeException ex){
-				//System.out.println("Wähle zwischen den 4 Möglichkeiten");
-				//}
-			//}while (!done);
-			// done=true;
-			//finally {
-			//getName();	
-			//}
-			
 			catch (Exception e){
 				System.out.println("Das kannst du nicht tun");
 				System.out.println(e);
