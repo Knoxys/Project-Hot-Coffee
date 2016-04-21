b882002131d9df91fe0d230541828683e8f77a02
mharnisc <mharnisc@MC00023631.one.steria.dom>
2016-04-21 15:02:17 +0200

    Commit

--- Project Hot Coffee/src/TicketSystem.java
+++ Project Hot Coffee/src/TicketSystem.java
@@ -1,26 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 	
 	public class TicketSystem{
 	
-		// 1) get name
-		// 2) select movie
-		// 3) select seat
-		// 4) print out
-		
-		
 		 // Beispiel für die Methode zu Eingabe Des Namen und zur ausführung des Namen
 		 // Desweiteren muss static hinter Scanner, weil Public main Void
 		 // https://www.youtube.com/watch?v=5QzyHxv-XxI
 
 		static Scanner scan = new Scanner(System.in);
 		static String name;
-		
-		// Nummer Eingabe ohne String Exception
-		
-		static String[] stichwort = {"1) Blackscreen","2) Exception","3) Nullpointer","4) Anders Problem bzw. Neues Ticket eröffnen"};
 		static int nummer;
-			
+		static String[] stichwort = {"1) Blackscreen","2) Exception","3) Nullpointer","4) Anders Problem bzw. Neues Ticket eröffnen"};
+		
 		public static void getName(){
 			System.out.println("Bitte gib dein Name ein");
 			name=scan.nextLine();
@@ -30,40 +22,33 @@ import java.util.Scanner;
 
 		public static void getStichwort(){
 			System.out.println("Bitte wähl ein Stichwort");
+			//http://stackoverflow.com/questions/35471870/infinite-loop-while-catching-an-exception
 			boolean done = false;
-			while(!done)
-			  {
-			     try
-			     {
-			    	 
+			while(!done){
+			     	try{
 			    	 for (int i=0;i<stichwort.length;i++){
-			 			System.out.println(stichwort[i]);
-			 			}
-			    	 
-			 			int nummer=scan.nextInt();
-			 			System.out.println("Du hast gewähl "+stichwort[nummer-1].substring(3,stichwort[nummer-1].length()));
-			 			done=true;
-			 			
-			        
-			     }
-			     catch(InputMismatchException e)
-			     {
-			        System.out.println("Ich Brauch eine Zahl");
-			        break;
-			     }
-			  }
-			}
+			 		System.out.println(stichwort[i]);
+			    	 }
+			 		int nummer=scan.nextInt();
+			 		System.out.println("Du hast gewähl "+stichwort[nummer-1].substring(3,stichwort[nummer-1].length()));
+			 		done=true;
+			     	}
+			     	catch(InputMismatchException e){
+			    	System.out.println("Ich Brauch eine Zahl");
+			    	break;
+			     	}
+			}	
+		}
 
 		public static void main(String [] args) {
 			
 			try{
-			getName();
+			getName();	
+			}
 			
-				
-			}
 			catch (Exception e){
-				System.out.println("Das kannst du nicht tun");
-				System.out.println(e);
-			}
+			System.out.println("Das kannst du nicht tun");
+			System.out.println(e);
 			}
 		}
+	}
