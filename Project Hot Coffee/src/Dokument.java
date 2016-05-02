ef57bb8fdab15c609d1262eb554a7f746dbce5c3
211151004 <211151004@RZS10904>
2016-05-02 11:33:39 +0200

    Fortschitt mit 2. Methode für Buffer

--- Project Hot Coffee/src/Dokument.java
+++ Project Hot Coffee/src/Dokument.java
@@ -1,11 +1,20 @@
+//1. Methode
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.*;
 
+//2. Methode
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Dokument {
 	public static void main(String args[]) throws IOException {
 
-		//1. 
+		//1. METHODE
 		File file = new File("Hello1.txt");
 		// creates the file
 		file.createNewFile();
@@ -23,6 +32,37 @@ public class Dokument {
 		for (char c : a)
 			System.out.print(c); // prints the characters one by one
 		fr.close();
+		//1. METHODE ENDE
+		
+		//2.Methode
+		
+
+			try {
+				mybean mb = new mybean("first value", "second value");
+
+				// write object to file
+				FileOutputStream fos = new FileOutputStream("mybean.ser");
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(mb);
+				oos.close();
+
+				// read object from file
+				FileInputStream fis = new FileInputStream("mybean.ser");
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				mybean result = (mybean) ois.readObject();
+				ois.close();
+
+				//http://avajava.com/tutorials/lessons/how-do-i-write-an-object-to-a-file-and-read-it-back.html
+				System.out.println("One:" + result.getOne() + ", Two:" + result.getTwo());
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-}
+
