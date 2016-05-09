14dee51df5e096aa10691d8c3ecca618ddadfa0d
Knoxys <Knoxys@25.64.152.187>
2016-05-09 10:41:33 +0200

    More Polishing

--- Project Hot Coffee/src/TicketSystem.java
+++ Project Hot Coffee/src/TicketSystem.java
@@ -4,19 +4,19 @@ public class TicketSystem {
 
 		try {
 
-			Benutzer 			b = new Benutzer();
-			Wahl 	 			w = new Wahl();
-			DBschreiben 		dbschreiben = new DBschreiben();
-			Zeit 	 			z = new Zeit();
-			DBlesen 			dblesen = new DBlesen();
-			// Dokument d = new Dokument();
+			Benutzer 		b 			= new Benutzer();
+			Wahl 	 		w 			= new Wahl();
+			Zeit 	 		z 			= new Zeit();
+			DBschreiben 	dbschreiben = new DBschreiben();
+			DBlesen 		dblesen 	= new DBlesen();
+			
 
 			b.setName();
 			w.setStichwort();
 			z.setZeitStempel();
 			dbschreiben.setDB();
 			dblesen.getDB();
-			// d.setDokument();
+			
 
 		}
 
